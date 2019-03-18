58d919dba51c9abd0d4cb1c732134e7401691541
Eduardo Martins Duarte <30420263+Eduardo-MD@users.noreply.github.com>
2019-03-18 19:54:53 -0300

    Add files via upload

---
+++ Site 1/WebContent/_js/script.js
@@ -0,0 +1,11 @@
+var $doc = $('html, body');
+        $('a').click(function() {
+            $doc.animate({
+                scrollTop: $( $.attr(this, 'href')).offset().top
+            }, 700);
+            return false;
+});
+
+function show_alert() {
+    alert("Mensagem enviada com sucesso!");
+}
